50ad465dde1b1d6eaa78d788351e293e0a3e9156
iohack <iohack@192.168.1.44>
2026-07-19 12:56:52 +0200

    Packages reorganization

--- src/test/java/fr/vborg/sudoku/sudoku/game/SudokuGameTest.java
+++ src/test/java/fr/vborg/sudoku/sudoku/game/SudokuGameTest.java
@@ -1,4 +1,4 @@
-package fr.vborg.sudoku.rule;
+package fr.vborg.sudoku.sudoku.game;
 
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertEquals;

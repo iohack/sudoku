3c62473eddcdc3f80e1103d968528bedca1aaa17
iohack <iohack@192.168.1.44>
2026-07-19 15:41:13 +0200

    Creation : user setting (or emptying) a case

---
+++ src/main/java/fr/vborg/sudoku/game/Move.java
@@ -0,0 +1,122 @@
+package fr.vborg.sudoku.game;
+
+import java.util.Objects;
+
+import fr.vborg.sudoku.localization.Messages;
+
+/**
+ * Represents a Sudoku move entered by the user.
+ *
+ * @since 1.0
+ */
+public final class Move
+{
+	
+	 /**
+     * Message key used for string representation.
+     */
+    private static final String FORMAT_KEY =
+            "move.format";
+    
+    private final int row;
+    private final int column;
+    private final int value;
+
+
+    /**
+     * Creates a move.
+     *
+     * @param row the row index
+     * @param column the column index
+     * @param value the value to insert
+     */
+    public Move(
+            final int row,
+            final int column,
+            final int value)
+    {
+        this.row = row;
+        this.column = column;
+        this.value = value;
+    }
+
+
+    /**
+	 * Returns the row index.
+     * @return the row index
+     */
+    public int getRow()
+    {
+        return row;
+    }
+
+
+    /**
+     * Returns the column index.
+     * @return the column index
+     */
+    public int getColumn()
+    {
+        return column;
+    }
+
+    
+    /**
+     * Returns the cell value.
+     * @return the cell value
+     */
+    public int getValue()
+    {
+        return value;
+    }
+
+
+    @Override
+    public boolean equals(final Object obj)
+    {
+        if (this == obj)
+        {
+            return true;
+        }
+
+        if (!(obj instanceof Move))
+        {
+            return false;
+        }
+
+        Move other = (Move) obj;
+
+        return row == other.row
+                && column == other.column
+                && value == other.value;
+    }
+
+
+    @Override
+    public int hashCode()
+    {
+        return Objects.hash(
+                row,
+                column,
+                value);
+    }
+
+    /**
+     * 
+     * @return
+     */
+    public String format()
+    {
+        return Messages.get(
+        		FORMAT_KEY,
+                row,
+                column,
+                value);
+    }
+    
+    @Override
+    public String toString()
+    {
+    	return format();
+    }
+}
---
+++ src/test/java/fr/vborg/sudoku/sudoku/game/MoveTest.java
@@ -0,0 +1,61 @@
+package fr.vborg.sudoku.sudoku.game;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.Locale;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+
+import fr.vborg.sudoku.game.Move;
+import fr.vborg.sudoku.localization.Messages;
+
+class MoveTest
+{
+    @AfterEach
+    void restoreLocale()
+    {
+        Messages.setLocale(Locale.FRENCH);
+    }
+
+
+    @Test
+    void shouldFormatMoveInFrench()
+    {
+        Messages.setLocale(Locale.FRENCH);
+
+        Move move = new Move(1, 2, 3);
+
+        assertEquals(
+                "Coup[ligne=1, colonne=2, valeur=3]",
+                move.format());
+    }
+
+
+    @Test
+    void shouldFormatMoveInEnglish()
+    {
+        Messages.setLocale(Locale.ENGLISH);
+
+        Move move = new Move(1, 2, 3);
+
+        assertEquals(
+                "Move[row=1, column=2, value=3]",
+                move.format());
+    }
+
+
+    @Test
+    void shouldBeEqualWhenValuesAreIdentical()
+    {
+        Move first =
+                new Move(1, 2, 3);
+
+        Move second =
+                new Move(1, 2, 3);
+
+        assertEquals(
+                first,
+                second);
+    }
+}
